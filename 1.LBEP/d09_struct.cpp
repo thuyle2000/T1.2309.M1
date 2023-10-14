3088632f5197d108a179321c16deffc54acf682b
Le Mong Thuy <thuyle2000@gmail.com>
2023-10-14 10:37:42 +0700

    Upload Oct 14, 2023

--- 1.LBEP/d09_struct.cpp
+++ 1.LBEP/d09_struct.cpp
@@ -18,7 +18,7 @@ void print(struct STUDENT sv);
 int main(){
     system("cls");
 
-    //khai bao bien sv1 va sv2, co kieu [struct STUDENT]
+    //khai bao bien sv1, sv2 co kieu [struct STUDENT]
     struct STUDENT sv1 = {"sv01","son phi long", 2006, 70};
     struct STUDENT sv2;
     strcpy(sv2.id , "sv02");
@@ -26,9 +26,18 @@ int main(){
     sv2.yob = 2008;
     sv2.mark = 80;
 
-    // in noi dung cua sv1 va sv2
+    //tiep tuc khai bao bien sv3 co kieu [struct STUDENT]
+    struct STUDENT sv3;
+    printf("nhap ma so sv: "); scanf("%s", sv3.id);
+    fseek(stdin, 0, SEEK_END);
+    printf("nhap ho ten: "); scanf("%[^\n]", sv3.name);
+    printf("nhap nam sinh: "); scanf("%d", &sv3.yob);
+    printf("nhap diem cuoi khoa: "); scanf("%d", &sv3.mark);
+
+    // in noi dung cua sv1 va sv2, sv3
     print(sv1);
     print(sv2);
+    print(sv3);
 
 }
 
